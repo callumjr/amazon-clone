d4ce37a9b6f69c653333d3e89366b679e41fc366
callumjr <callumrosier.dev@gmail.com>
2022-02-01 21:46:25 +0000

    Nav bar responsive on for all screen sizes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 class DropDownMenu {
-	showPopUp(element) {
+	show(element) {
 		document.querySelector(element).classList.add('opacity-100');
 	}
 
 	hide(element) {
 		document.querySelector(element).classList.remove('opacity-100');
+	}
+
+	addZIndex(element) {
+		document.querySelector(element).classList.add('z-30');
+	}
+
+	removeZIndex(element) {
+		document.querySelector(element).classList.remove('z-30');
 	}
 }
 
@@ -14,20 +22,28 @@ const dropDownMenu = new DropDownMenu();
 
 document.querySelector('#country-container').addEventListener('mouseover', e => {
 	e.preventDefault();
-	dropDownMenu.showPopUp('#country-overlay');
+	dropDownMenu.show('#country-overlay');
 });
 
-document.addEventListener('mouseout', function(e) {
+document.addEventListener('mouseout', e => {
 	if (!document.querySelector('#country-dropdown').contains(e.target)) {
 		dropDownMenu.hide('#country-overlay');
 	}
+});
+
+document.querySelector('#country-container').addEventListener('mouseover', () => {
+	dropDownMenu.addZIndex('#country-container');
+});
+
+document.querySelector('#country-container').addEventListener('mouseout', () => {
+	dropDownMenu.removeZIndex('#country-container');
 });
 
 // Sign in dropdown
 
 document.querySelector('#sign-in-container').addEventListener('mouseover', e => {
 	e.preventDefault();
-	dropDownMenu.showPopUp('#country-overlay');
+	dropDownMenu.show('#country-overlay');
 });
 
 document.addEventListener('mouseout', function(e) {
@@ -36,17 +52,25 @@ document.addEventListener('mouseout', function(e) {
 	}
 });
 
+document.querySelector('#sign-in-container').addEventListener('mouseover', () => {
+	dropDownMenu.addZIndex('#sign-in-container');
+});
+
+document.querySelector('#sign-in-container').addEventListener('mouseout', () => {
+	dropDownMenu.removeZIndex('#sign-in-container');
+});
+
 class PopUpMenu {
-	showPopUp(popUp) {
+	show(popUp) {
 		document.querySelector(popUp).classList.remove('hidden');
 
 		document.querySelector('#overlay').classList.add('opacity-100');
 	}
 
-	hide(popUp) {
+	hide(popUp, overlay) {
 		document.querySelector(popUp).classList.add('hidden');
 
-		document.querySelector('#overlay').classList.remove('opacity-100');
+		document.querySelector(overlay).classList.remove('opacity-100');
 	}
 }
 
@@ -54,7 +78,7 @@ const popUpMenu = new PopUpMenu();
 
 document.querySelector('#address-container').addEventListener('click', e => {
 	e.preventDefault();
-	popUpMenu.showPopUp('#address-popup', '#overlay');
+	popUpMenu.show('#address-popup', '#overlay');
 });
 
 document.addEventListener('mouseup', function(e) {
@@ -62,6 +86,9 @@ document.addEventListener('mouseup', function(e) {
 		!document.querySelector('#address-popup').classList.contains('hidden') &&
 		!document.querySelector('#address-popup-div').contains(e.target)
 	) {
-		popUpMenu.hide('#address-popup');
+		popUpMenu.hide('#address-popup', '#overlay');
 	}
 });
+
+// three lines - amazon - sign in - cart -
+// search
