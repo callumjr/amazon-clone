981dc549ce822a79bd85d087ffe59c7f37cc507b
callumjr <callumrosier.dev@gmail.com>
2022-02-05 01:23:38 +0000

    cart has storing functionality

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,24 @@
-class Cart {
+class buildCart {
 	constructor() {
 		this.cart = [];
 	}
 
 	addToCart(element) {
-		{
-			productName: element.querySelector('');
-		}
+		this.cart.push({
+			name  : element.querySelector('#product-name').textContent,
+			image : element.querySelector('#product-img').getAttribute('src'),
+			price : element.querySelector('#product-price').textContent
+		});
+
+		document.querySelector('#cart-items').innerHTML = `${this.cart.length}`;
 	}
 }
+
+const cart = new buildCart();
+
+document.querySelectorAll('#cart-btn').forEach(v => {
+	v.addEventListener('click', e => {
+		e.preventDefault();
+		cart.addToCart(v.parentElement);
+	});
+});
