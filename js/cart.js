676f809cb2068034f60eda971e86c4a31a84ebe0
callumjr <callumrosier.dev@gmail.com>
2022-02-07 23:15:54 +0000

    address pop up ajusts to smaller screen sizes

--- js/cart.js
+++ js/cart.js
@@ -23,12 +23,22 @@ document.querySelectorAll('#cart-btn').forEach(v => {
 
 console.log(JSON.parse(window.localStorage.getItem('cart')));
 
-////////////////////////////////////////////////////////
-
 if (window.localStorage.getItem('cart')) {
 	document.querySelector('#cart-items').innerHTML = `${JSON.parse(window.localStorage.getItem('cart')).length}`;
 }
 
 class CartPage {
 	constructor() {}
+
+	cartHtml() {
+		if (window.localStorage.getItem('cart')) {
+			let cartArr = JSON.parse(window.localStorage.getItem('cart'));
+
+			for (let i = 0; i < cartArr.length; i++) {
+				document.querySelector('#cart-items').innerHTML += ``;
+			}
+		} else {
+			document.querySelector('#no-cart-items').classList.remove('hidden');
+		}
+	}
 }
