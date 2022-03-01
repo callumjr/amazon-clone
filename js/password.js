dc86f9e5489e556603e9c87002ba33bd7b24bacb
callumjr <callumrosier.dev@gmail.com>
2022-03-01 00:49:10 +0000

    password validation added

--- js/password.js
+++ js/password.js
@@ -1,3 +1,28 @@
+const invalidPassword = document.querySelector('#invalid-password');
+const signInButton = document.querySelector('#sign-in-button');
+const passwordBox = document.querySelector('#password');
+
+class Password {
+	passwordValidation(value) {
+		if (/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}/.test(value)) {
+			if (!invalidPassword.classList.contains('hidden')) {
+				invalidPassword.classList.add('hidden');
+			}
+		} else {
+			invalidPassword.classList.remove('hidden');
+		}
+	}
+}
+
+const password = new Password();
+
 const emailInsert = document.querySelector('.email-insert');
 
-emailInsert.innerHTML = `<span>${window.sessionStorage.getItem('email')}</span>`;
+if (window.sessionStorage.getItem('email') !== null) {
+	emailInsert.innerHTML = `<span>${window.sessionStorage.getItem('email')}</span>`;
+}
+
+signInButton.addEventListener('click', e => {
+	e.preventDefault();
+	password.passwordValidation(passwordBox.value);
+});
